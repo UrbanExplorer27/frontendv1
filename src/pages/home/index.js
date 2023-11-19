5830b9ba9aa8489ec066e98d7655355488bf26ee
UrbanExplorer27 <castlerokmarketing@gmail.com>
2023-11-19 01:30:47 -0700

    feat: a simple enter

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Chatbot from "../chatbot";
 
 const Home = () => {
+  
   return (
     <main className="flex flex-col h-full md:py-10 py-4 px-2 md:px-0">
       <section className="flex-grow">
